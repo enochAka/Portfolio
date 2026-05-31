de4831fd7c07604d1da102cbb313ce8187fc9151
Enoch Akande <enochakande12@gmail.com>
2026-05-31 16:35:35 +0100

    Finished desktop version of website

--- script.js
+++ script.js
@@ -7,18 +7,44 @@ window.addEventListener("load", () => {
     const quickActionButtons = document.getElementById("quick-action");
     const scrollPrompt = document.getElementById("scroll-prompt");
     const name = document.getElementById("name");
+    const aboutMeDiv = document.getElementById("about-me");
+    const projectsDiv = document.getElementById("projects");
+    const contactDiv = document.getElementById("contact");
+    const spaceDiv = document.getElementById("space");
+
     availableDiv.style.display = "none";
     introductionHeading.style.display = "none";
     descriptionSubHeading.style.display = "none";
     descriptionParagraph.style.display = "none";
     quickActionButtons.style.display = "none";
     scrollPrompt.style.display = "none";
+    aboutMeDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    spaceDiv.style.height = "100vh";
+    
+
     setTimeout(() => {availableDiv.style.animation = "float-in 1s ease-in-out forwards"; availableDiv.style.display = "flex"}, 100);
     setTimeout(() => {introductionHeading.style.animation = "float-in 1s ease-in-out forwards"; introductionHeading.style.display = "block";}, 200);
     setTimeout(() => {descriptionSubHeading.style.animation = "float-in 1s ease-in-out forwards"; descriptionSubHeading.style.display = "flex"}, 300);
     setTimeout(() => {descriptionParagraph.style.animation = "float-in 1s ease-in-out forwards"; descriptionParagraph.style.display = "flex"}, 400);
     setTimeout(() => {quickActionButtons.style.animation = "float-in 1s ease-in-out forwards"; quickActionButtons.style.display = "flex"}, 500);
     setTimeout(() => {scrollPrompt.style.animation = "float-in 1s ease-in-out forwards"; scrollPrompt.style.display = "flex"}, 600);
-    setTimeout(() => { name.style.animation = "intensified-glow 1s ease-in-out forwards";}, 700)
     
+    window.addEventListener("scroll", () => {
+        const scrollAmount = window.scrollY;
+        if (scrollAmount > 500) {
+            aboutMeDiv.style.animation = "float-in 1s ease-in-out forwards";
+            aboutMeDiv.style.display = "block";
+        }
+        if (scrollAmount > 1300) {
+            projectsDiv.style.animation = "float-in 1s ease-in-out forwards";
+            projectsDiv.style.display = "block";
+        }
+        if (scrollAmount > 2200) {
+            contactDiv.style.animation = "float-in 1s ease-in-out forwards";
+            contactDiv.style.display = "flex";
+            spaceDiv.style.height = "0";
+        }
+    })
 })
