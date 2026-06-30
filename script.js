3153d2c47e36ba6e58764093237432dc4f650882
Enoch Akande <enochakande12@gmail.com>
2026-06-30 12:23:14 +0100

    fixed the moible veiw

--- script.js
+++ script.js
@@ -1,50 +1,113 @@
 window.addEventListener("load", () => {
-    const availableDiv = document.getElementById("availability");
-    const navigation = document.getElementsByTagName("nav");
-    const introductionHeading = document.getElementById("greeting");
-    const descriptionSubHeading = document.getElementById("description");
-    const descriptionParagraph = document.getElementById("description-detail");
-    const quickActionButtons = document.getElementById("quick-action");
-    const scrollPrompt = document.getElementById("scroll-prompt");
-    const name = document.getElementById("name");
-    const aboutMeDiv = document.getElementById("about-me");
-    const projectsDiv = document.getElementById("projects");
-    const contactDiv = document.getElementById("contact");
-    const spaceDiv = document.getElementById("space");
+  const mobileNav = document.getElementById("mobile-nav");
+  const mobileIcon = document.getElementById("iconMobile");
+  const availableDiv = document.getElementById("availability");
+  const navigation = document.getElementsByTagName("nav");
+  const introductionHeading = document.getElementById("greeting");
+  const descriptionSubHeading = document.getElementById("description");
+  const descriptionParagraph = document.getElementById("description-detail");
+  const quickActionButtons = document.getElementById("quick-action");
+  const scrollPrompt = document.getElementById("scroll-prompt");
+  const name = document.getElementById("name");
+  const aboutMeDiv = document.getElementById("about-me");
+  const projectsDiv = document.getElementById("projects");
+  const contactDiv = document.getElementById("contact");
+  const spaceDiv = document.getElementById("space");
+  const menuButton = document.getElementById("menu-icon-container");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const closeIcon = document.getElementById("close-icon-container");
 
-    availableDiv.style.display = "none";
-    introductionHeading.style.display = "none";
-    descriptionSubHeading.style.display = "none";
-    descriptionParagraph.style.display = "none";
-    quickActionButtons.style.display = "none";
-    scrollPrompt.style.display = "none";
-    aboutMeDiv.style.display = "none";
-    projectsDiv.style.display = "none";
-    contactDiv.style.display = "none";
-    spaceDiv.style.height = "100vh";
-    
+  availableDiv.style.display = "none";
+  introductionHeading.style.display = "none";
+  descriptionSubHeading.style.display = "none";
+  descriptionParagraph.style.display = "none";
+  quickActionButtons.style.display = "none";
+  scrollPrompt.style.display = "none";
+  aboutMeDiv.style.display = "none";
+  projectsDiv.style.display = "none";
+  contactDiv.style.display = "none";
+  spaceDiv.style.height = "100vh";
 
-    setTimeout(() => {availableDiv.style.animation = "float-in 1s ease-in-out forwards"; availableDiv.style.display = "flex"}, 100);
-    setTimeout(() => {introductionHeading.style.animation = "float-in 1s ease-in-out forwards"; introductionHeading.style.display = "block";}, 200);
-    setTimeout(() => {descriptionSubHeading.style.animation = "float-in 1s ease-in-out forwards"; descriptionSubHeading.style.display = "flex"}, 300);
-    setTimeout(() => {descriptionParagraph.style.animation = "float-in 1s ease-in-out forwards"; descriptionParagraph.style.display = "flex"}, 400);
-    setTimeout(() => {quickActionButtons.style.animation = "float-in 1s ease-in-out forwards"; quickActionButtons.style.display = "flex"}, 500);
-    setTimeout(() => {scrollPrompt.style.animation = "float-in 1s ease-in-out forwards"; scrollPrompt.style.display = "flex"}, 600);
-    
-    window.addEventListener("scroll", () => {
-        const scrollAmount = window.scrollY;
-        if (scrollAmount > window.innerHeight * 0.5) {
-            aboutMeDiv.style.animation = "float-in 1s ease-in-out forwards";
-            aboutMeDiv.style.display = "block";
-        }
-        if (scrollAmount > window.innerHeight * 1.5) {
-            projectsDiv.style.animation = "float-in 1s ease-in-out forwards";
-            projectsDiv.style.display = "block";
-        }
-        if (scrollAmount > window.innerHeight * 3.5) {
-            contactDiv.style.animation = "float-in 1s ease-in-out forwards";
-            contactDiv.style.display = "flex";
-            spaceDiv.style.height = "0";
-        }
-    })
-})
+  setTimeout(() => {
+    availableDiv.style.animation = "float-in 1s ease-in-out forwards";
+    availableDiv.style.display = "flex";
+  }, 100);
+  setTimeout(() => {
+    introductionHeading.style.animation = "float-in 1s ease-in-out forwards";
+    introductionHeading.style.display = "block";
+  }, 200);
+  setTimeout(() => {
+    descriptionSubHeading.style.animation = "float-in 1s ease-in-out forwards";
+    descriptionSubHeading.style.display = "flex";
+  }, 300);
+  setTimeout(() => {
+    descriptionParagraph.style.animation = "float-in 1s ease-in-out forwards";
+    descriptionParagraph.style.display = "flex";
+  }, 400);
+  setTimeout(() => {
+    quickActionButtons.style.animation = "float-in 1s ease-in-out forwards";
+    quickActionButtons.style.display = "flex";
+  }, 500);
+  setTimeout(() => {
+    scrollPrompt.style.animation = "float-in 1s ease-in-out forwards";
+    scrollPrompt.style.display = "flex";
+  }, 600);
+
+  function spawnDot() {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+
+    dot.style.left = `${x}px`;
+    dot.style.top = `${y} px`;
+
+    document.getElementById("dots").appendChild(dot);
+
+    setTimeout(
+      () => {
+        dot.style.transition = "opacity 1s";
+        dot.style.opacity = 0;
+
+        setTimeout(() => dot.remove(), 1000);
+      },
+      3000 + Math.random() * 3000,
+    );
+  }
+
+  window.addEventListener("scroll", () => {
+    const scrollAmount = window.scrollY;
+    if (scrollAmount > window.innerHeight * 0.2) {
+      aboutMeDiv.style.animation = "float-in 1s ease-in-out forwards";
+      aboutMeDiv.style.display = "block";
+    }
+    if (scrollAmount > window.innerHeight * 1.2) {
+      projectsDiv.style.animation = "float-in 1s ease-in-out forwards";
+      projectsDiv.style.display = "block";
+    }
+    if (scrollAmount > window.innerHeight * 2.5) {
+      contactDiv.style.animation = "float-in 1s ease-in-out forwards";
+      contactDiv.style.display = "flex";
+      spaceDiv.style.height = "0";
+    }
+  });
+
+  menuButton.addEventListener("click", () => {
+    mobileMenu.style.display = "flex";
+    mobileMenu.style.animation = "rollDownNav 1s ease-in-out";
+    menuButton.classList.add("fade-out");
+    closeIcon.classList.remove("fade-out");
+    mobileIcon.classList.add("fade-out");
+    closeIcon.addEventListener("click", () => {
+        closeIcon.classList.add("fade-out")
+        menuButton.classList.remove("fade-out")
+        mobileMenu.style.animation = "rollUpNav 1s ease-in-out";
+        mobileIcon.classList.remove("fade-out");
+        setTimeout(() => {
+        mobileMenu.style.display = "none";
+        }, 1000);
+    });
+  });
+  
+});
